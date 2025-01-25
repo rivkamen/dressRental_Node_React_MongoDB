21b4a28d872dc4ca8e84e1bab2979b9afdba412c
rivkamen <159689577+rivkamen@users.noreply.github.com>
2025-01-25 20:09:45 +0200

    page buton hover

--- server/models/DressDesign.js
+++ server/models/DressDesign.js
@@ -1,23 +1,3 @@
-//  const mongoose = require('mongoose');
-// const dressSchema = require('./Dress');
-// const Schema = mongoose.Schema;
-
-// const dressDesignSchema = new Schema({
-// name:{type:String, required:true},
-// description:{type:String},
-// images: { 
-//     type: [String]
-// }, 
-// dressListSizes: [{
-//     key: {type:String},
-//     dresses: [dressSchema],
-//     size:{type:String,
-//         enum:["women", "girls"],
-//         required:true
-
-//     },
-// }]})
-// module.exports = mongoose.model('Dress', dressDesignSchema);
 
 const mongoose = require('mongoose');
 const dressSchema = require('./Dress');
@@ -39,7 +19,5 @@ const dressDesignSchema = new Schema({
 }, {
   timestamps: true
 });
-// const DressDesign = mongoose.model('Dress', dressDesignSchema);
-// DressDesign.createIndexes(); // יצירת אינדקסים
 
  module.exports = mongoose.model('Dress', dressDesignSchema);
